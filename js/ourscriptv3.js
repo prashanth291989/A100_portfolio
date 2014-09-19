42b091193b30f12a5b65c392f5c0fdd8a506cb0c
Mark Fisher <mfishera100@gmail.com>
2014-09-18 20:35:15 -0400

    Changed Files

--- js/ourscriptv3.js
+++ js/ourscriptv3.js
@@ -1,14 +1,28 @@
-var array = [ 
+var imgarray = [ 
 	 "img/website_redesign.png",
 	 "img/website_revamp.png", 
-	 "img/student_app.png", 
-	 "img/job_board.png" ];
+	 "img/Proto_App.png", 
+	 "img/whiteboard_head.png" ];
 
-$("#insert_imgs").html('<image src="'+array[0]+'"+>')
+
+$("#insert_imgs").html('<image src="'+imgarray[0]+'"+>')
+$('#projText').hide();
+var u = $("ul.testText li:eq('"+0+"')").text();
+$("#insert_text").html('<p>"'+u+'"</p>');
+
 
 $("a.listen").click(function() {
 
-	var i = array[$("a.listen").index(this)];
+	var i = imgarray[$("a.listen").index(this)];
 	$("#insert_imgs").html('<image src="'+i+'"+>');
 	console.log(i);
+
+	var x = $("a.listen").index(this);
+	console.log(x);
+	var z = $("ul.testText li:eq('"+x+"')").text();
+	$("ul.testText li:not('"+x+"')").hide();
+	$("#insert_text").html('<p>"'+z+'"</p>');
+	
+	console.log(z);
 });
+
